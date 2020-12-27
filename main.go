6be638e129bf4b2ff95659a35b44292ef09105eb
ThatOneTallKid <1905222@kiit.ac.in>
2020-12-27 23:05:04 +0530

    commented

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", SimpleServer)
+	http.ListenAndServe(":3000", nil)
+
+}
+
+// SimpleServer is ...
+func SimpleServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
 }
