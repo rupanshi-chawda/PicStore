2c6e0f6d61c505d75c30db9dd4a50cf13ce5f7c2
ThatOneTallKid <1905222@kiit.ac.in>
2020-12-28 13:33:46 +0530

    Connection established

--- main.go
+++ main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	http.HandleFunc("/", SimpleServer)
-	http.ListenAndServe(":3000", nil)
+	// http.HandleFunc("/", SimpleServer)
+	// http.ListenAndServe(":3000", nil)
+	Connect()
 
 }
 
 // SimpleServer is ...
 func SimpleServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+}
+
+// Connect to MongoDB server
+func Connect() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to MongoDB!")
 }
