3d15fee058fcfc85e864979f73ae29ba8ca8c00a
ThatOneTallKid <1905222@kiit.ac.in>
2020-12-31 11:45:11 +0530

    Front end and backend

---
+++ backend/go.mod
@@ -0,0 +1,8 @@
+module github.com/ThatOneTallKid/PicStore
+
+go 1.15
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	go.mongodb.org/mongo-driver v1.4.4
+)
--- backend/main.go
+++ backend/main.go
@@ -26,7 +26,7 @@ func main() {
 	UploadFile(file, filename)
 	//DownloadFile(filename)
 }
-
+	
 // SimpleServer is ...
 func SimpleServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
