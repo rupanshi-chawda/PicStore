55099d239a7e55916b58c734c05d23bb1a6ceb61
ThatOneTallKid <1905222@kiit.ac.in>
2021-01-01 11:36:08 +0530

    blobs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <Hero />
         <Switch>
           <Route path='/' exact/>
         </Switch>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
@@ -8,7 +8,7 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        Pictore <i className = 'fab fa-typo3' />   
+                        PicStore <i className = 'fab fa-typo3' />   
                     </Link>
                 </div>
             </nav>
